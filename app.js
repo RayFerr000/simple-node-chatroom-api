5e11c6d225106f93c1584bc577810df8db2f8bc4
Ray <rayferranti@Gmail.com>
2020-02-15 19:43:16 -0500

    Setup postgres connection

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
+
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+app.get('/db', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT * FROM test_table');
+    const results = { 'results': (result) ? result.rows : mull };
+    res.send('THESE ARE THE QUERY RESULTS ' + results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send('Error ' + err);
+  }
+})
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
